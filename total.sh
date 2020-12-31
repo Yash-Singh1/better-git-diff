636fb2f82e307483033ab8b7fb76692ecf2c6a4e
Yash Singh <saiansh2525@gmail.com>
2020-12-30 20:27:58 -0800

    Remove binaries and remove MODIFIED_FILES variable

--- total.sh
+++ total.sh
@@ -1,6 +1,5 @@
 INSERTIONS=`git diff --stat | sed '$!d ; s/.*changed,[[:space:]]// ; s/[[:space:]]insertions.*//'`
-CREATIONS=`expr \`for i in $(git ls-files -o  --exclude-standard | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d'); do wc -l $i | sed 's/[[:space:]].*//g'; done | awk 'ORS=" + "' | head -c -1\` 0`
-CREATED_FILES=`git ls-files -o  --exclude-standard | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d' | awk '{ print ln++ }' | sed '$!d'`
-MODIFIED_FILES=`git diff --stat | sed '$!d ; s/,.*// ; s/[[:space:]]// ; s/[[:space:]].*//'`
+CREATIONS=`expr \`for i in $(git ls-files -o  --exclude-standard | sed -E '/.*\.(png|zip)/d' | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d'); do wc -l $i | sed 's/[[:space:]].*//g'; done | awk 'ORS=" + "' | head -c -1\` 0`
+CREATED_FILES=`git ls-files -o  --exclude-standard | sed -E '/.*\.(png|zip)/d' | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d' | awk '{ print ln++ }' | sed '$!d'`
 
 git diff --color=always --stat $@ | sed "s/${INSERTIONS}/$(expr $INSERTIONS + $CREATIONS)/ ; s/changed,/changed, ${CREATED_FILES} files created,/"
