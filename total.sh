5d902af14659ea875108de8ca47191a903b13992
Yash-Singh1 <saiansh2525@gmail.com>
2022-01-20 19:09:22 -0800

    fix: no files created

--- total.sh
+++ total.sh
@@ -1,5 +1,6 @@
 INSERTIONS=`git diff --stat | sed '$!d ; s/.*changed,[[:space:]]// ; s/[[:space:]]insertions.*//'`
 CREATIONS=`expr \`for i in $(git grep -I --name-only --untracked -e . -- $(git ls-files -o  --exclude-standard | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d')); do wc -l $i | sed 's/[[:space:]].*//g'; done | awk 'ORS=" + "' | head -c -1\` 0`
-CREATED_FILES=`git grep -I --name-only --untracked -e . -- $(git ls-files -o  --exclude-standard | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d') | awk '{ print ln++ }' | sed '$!d'`
+MATCHED_FILES=`git ls-files -o  --exclude-standard | sed '/search[[:space:]](for[[:space:]]N-th[[:space:]]occurrence)\./d'`
+CREATED_FILES=`git grep -I --name-only --untracked -e . -- "${MATCHED_FILES:-\!\*}" | cat | awk '{ print ln++ }' | sed '$!d'`
 
 git diff --color=always --stat $@ | sed "s/${INSERTIONS}/$(expr $INSERTIONS + $CREATIONS)/ ; s/changed,/changed, ${CREATED_FILES} files created,/"
